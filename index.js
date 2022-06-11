c717173ced858ac541ca165ec8ec0ae299d2439c
Emmanuel Omondi Aloo <aloo25emmanuel@gmail.com>
2022-06-11 17:14:35 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName= "bob";
+
+function upperCaseCustomerName() {
+
+     customerName="BOB"  
+  }console.log(customerName);
+
+
+  function setBestCustomer(){
+            bestCustomer='not bob';
+  }console.log(bestCustomer);
+
+  function overwriteBestCustomer(){
+
+     bestCustomer='maybe bob';
+  }console.log(bestCustomer);
+
+  const leastFavoriteCustomer= "Kimani"
+
+  function changeLeastFavoriteCustomer(){
+
+     leastFavoriteCustomer= "John"
+  }
+  console.log(leastFavoriteCustomer);
